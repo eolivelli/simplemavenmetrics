359481f041af032e1a27a8226d92aa5d0638ed8a
Enrico Olivelli <eolivelli@apache.org>
2020-04-13 14:57:44 +0200

    initial commit

---
+++ src/main/java/org/apache/maven/metrics/impl/SimpleMetricsProvider.java
@@ -0,0 +1,155 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.maven.metrics.impl;
+
+import java.util.Objects;
+import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.function.BiConsumer;
+import org.apache.maven.metrics.Counter;
+import org.apache.maven.metrics.Gauge;
+import org.apache.maven.metrics.MetricsContext;
+import org.apache.maven.metrics.MetricsProvider;
+import org.apache.maven.metrics.MetricsProviderLifeCycleException;
+import org.apache.maven.metrics.Summary;
+import org.apache.maven.metrics.SummarySet;
+import org.codehaus.plexus.logging.Logger;
+
+/**
+ * Default implementation of {@link MetricsProvider}.<br>
+ * It does not implement a real hierarchy of contexts, but metrics are flattened
+ * in a single namespace.<br>
+ */
+public class SimpleMetricsProvider implements MetricsProvider {
+
+    private final DefaultMetricsContext rootMetricsContext = new DefaultMetricsContext();
+    private final Logger log;
+
+    public SimpleMetricsProvider(Logger log) {
+        this.log = log;
+    }
+
+    @Override
+    public void start() throws MetricsProviderLifeCycleException {
+    }
+
+    @Override
+    public MetricsContext getRootContext() {
+        return rootMetricsContext;
+    }
+
+    @Override
+    public void stop() {
+        boolean dumpAtEnd = Boolean.getBoolean("metrics.dumpAtEnd");
+        if (dumpAtEnd) {
+            log.info("Maven Runtime Execution Metrics");
+            rootMetricsContext.dump((k, v) -> {
+                log.info(k + ":" + v);
+            });
+        }
+        // release all references to external objects
+        rootMetricsContext.gauges.clear();
+    }
+
+    @Override
+    public void resetAllValues() {
+        rootMetricsContext.reset();
+    }
+
+    private static final class DefaultMetricsContext implements MetricsContext {
+
+        private final ConcurrentMap<String, Gauge> gauges = new ConcurrentHashMap<>();
+        private final ConcurrentMap<String, SimpleCounter> counters = new ConcurrentHashMap<>();
+        private final ConcurrentMap<String, AvgMinMaxPercentileCounter> summaries = new ConcurrentHashMap<>();
+        private final ConcurrentMap<String, AvgMinMaxPercentileCounterSet> summarySets = new ConcurrentHashMap<>();
+
+        @Override
+        public MetricsContext getContext(String name) {
+            // no hierarchy yet
+            return this;
+        }
+
+        @Override
+        public Counter getCounter(String name) {
+            return counters.computeIfAbsent(name, (n) -> {
+                return new SimpleCounter(n);
+            });
+        }
+
+        @Override
+        public void registerGauge(String name, Gauge gauge) {
+            Objects.requireNonNull(gauge, "Cannot register a null Gauge for " + name);
+            gauges.put(name, gauge);
+        }
+
+        @Override
+        public void unregisterGauge(String name) {
+            gauges.remove(name);
+        }
+
+        @Override
+        public Summary getSummary(String name) {
+            return summaries.computeIfAbsent(name, (n) -> {
+                return new AvgMinMaxPercentileCounter(name);
+            });
+
+        }
+
+        @Override
+        public SummarySet getSummarySet(String name) {
+            return summarySets.computeIfAbsent(name, (n) -> {
+                return new AvgMinMaxPercentileCounterSet(name);
+            });
+
+        }
+
+        void dump(BiConsumer<String, Object> sink) {
+            gauges.forEach((name, metric) -> {
+                Number value = metric.get();
+                if (value != null) {
+                    sink.accept(name, value);
+                }
+            });
+            counters.values().forEach(metric -> {
+                metric.values().forEach(sink);
+            });
+            summaries.values().forEach(metric -> {
+                metric.values().forEach(sink);
+            });
+            summarySets.values().forEach(metric -> {
+                metric.values().forEach(sink);
+            });
+        }
+
+        void reset() {
+            counters.values().forEach(metric -> {
+                metric.reset();
+            });
+            summaries.values().forEach(metric -> {
+                metric.reset();
+            });
+            summarySets.values().forEach(metric -> {
+                metric.reset();
+            });
+            // no need to reset gauges
+        }
+
+    }
+
+}
